ed05779925553da330500701fbabc7f2855e7837
Subhra S Sarkar <rurtle.coder@gmail.com>
2025-12-10 11:07:01 +0000

    Leared about line and block comments. Yet to try the library comments though

---
+++ rust_by_example/01-hello_world/comments.rs
@@ -0,0 +1,13 @@
+fn main() {
+
+    // Here is a line comment
+
+    /** 
+     * Here we have some 
+     * block comment spread
+     * across multiple lines
+     */
+
+     let x = 5 + /* 90 + */ 5;
+     println!("Is `x` 10 or 100? {}", x);
+}
