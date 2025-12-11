bbd9e976510e2b70114cdfaee08c2998a1ad8a68
Subhra S Sarkar <rurtle.coder@gmail.com>
2025-12-11 06:08:19 +0000

    Finished the lesson about formatting today

--- rust_by_example/01-hello_world/formatting.rs
+++ rust_by_example/01-hello_world/formatting.rs
@@ -16,4 +16,20 @@ fn main() {
     println!("Base 2:                      {:b}", 9999999);
     println!("Base 8:                      {:o}", 9999999);
     println!("Base 16:                      {:x}", 9999999);
+
+    // Right justify with spaces
+    println!("{number:>8}", number = 1);
+    // Right justify with 0s
+    println!("{number:0>8}", number = 1);
+    // Left justify with 0s
+    println!("{number:0<8}", number = 1);
+    // Making it slightly more dynamic
+    println!("{number:0>width$}", number = 1, width = 8);
+
+    // Argument enumeration
+    println!("My name is {1}, {0} {1}", "James", "Bond");
+
+    let number: f64 = 1.0;
+    let width: usize = 8;
+    println!("{number:0>width$}");
 }
