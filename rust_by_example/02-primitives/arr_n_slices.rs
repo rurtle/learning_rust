3d10d2422d898fb7df77c7296d29526888ef3691
Subhra S Sarkar <rurtle.coder@gmail.com>
2026-01-08 06:55:06 +0000

    Finally finished arrays and slices - chapter 02 on Primitives

--- rust_by_example/02-primitives/arr_n_slices.rs
+++ rust_by_example/02-primitives/arr_n_slices.rs
@@ -1,5 +1,11 @@
 use std::mem;
 
+// This function borrows a slice
+fn inspect_slice(slice: &[i32]) {
+    println!("First element: {}", slice[0]);
+    println!("The slice has {} elements", slice.len());
+}
+
 fn main() {
     // Fixed size array. Type signature is superfluous
     let xs: [i32; 5] = [1, 2, 3, 4, 5];
@@ -9,6 +15,39 @@ fn main() {
 
     // Arary indexing starts at 0
     println!("First element: {}", ys[0]);
-    println!("Second element: {}", ys[1]);
+    println!("Second element: {}", xs[1]);
 
+    // `len` returns the count of elements in the array
+    println!("Number of elements in xs: {}", xs.len());
+    println!("Length of the array ys: {}", ys.len());
+
+    // Arrays are stack allocated
+    println!("Size of xs: {} bytes", mem::size_of_val(&xs));
+    println!("Size of ys: {} bytes", mem::size_of_val(&ys));
+
+    // Arrays can be automatically borrowed as slices
+    inspect_slice(&xs);
+    inspect_slice(&ys[0..100]);
+
+    // Example of empty slice
+    let empty_arr: [u32; 0] = [];
+    assert_eq!(&empty_arr, &[]);
+    assert_eq!(&empty_arr, &[][..]);
+
+    // Arrays can be safely accessed using `.get`, which returns an
+    // `Option`. This can be matched as shown below, or used with `.expect()`
+    // if you would like the program to exit with a nice message instead of
+    // happily continuing
+    for i in 0..xs.len() + 1{
+        match xs.get(i) {
+            Some(xval) => println!("{}: {}", i, xval),
+            None => println!("Slow down! {} is too far", i),
+        }
+    }
+
+    // out of bound indexing on array with constant value causes compile
+    // time error
+    // println!("{}", xs[5]);
+    // Out of bound indexing on slice causes runtime error
+    println!("{}", xs[..][7]);
 }
